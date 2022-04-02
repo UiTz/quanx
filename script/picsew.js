e56bd486992ff96ddc0b0e4c702b6cdfb07992bb
liujx <liujx18@asiainfo.com>
2022-04-02 16:32:22 +0800

    update picsew.js

--- script/picsew.js
+++ script/picsew.js
@@ -1,8 +1,8 @@
 /*
 #圈x
-https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/mandxy/ziyong/main/picsew.js
+https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/UiTz/quanx/master/script/picsew.js
 #Loon
-http-response https:\/\/buy\.itunes\.apple\.com\/verifyReceipt script-path=https://raw.githubusercontent.com/mandxy/ziyong/main/picsew.js, requires-body=true, timeout=10, tag=picsew专业版
+http-response https:\/\/buy\.itunes\.apple\.com\/verifyReceipt script-path=https://raw.githubusercontent.com/UiTz/quanx/master/script/picsew.js, requires-body=true, timeout=10, tag=picsew专业版
 #[mitm]
 hostname=buy.itunes.apple.com
 */
